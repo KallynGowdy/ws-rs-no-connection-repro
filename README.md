5478f48dd3d2d4ccd84739832d437af604de6ffb
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-06-24 01:27:24 -0400

    Make it better

--- README.md
+++ README.md
@@ -2,11 +2,23 @@ Repro for https://github.com/servo/servo/issues/27043
 
 ### Steps
 
-1. [Clone and build the servo repo](https://github.com/servo/servo).
-2. Ensure you have the [rust toolchain installed](https://rustup.rs/).
-3. Set the `OPENSSL_DIR` environment variable to the OpenSSL install directory created by the servo build.
+(appologies for the weird repro steps)
+
+1. Be running on Windows 10.
+2. [Clone and build the servo repo](https://github.com/servo/servo).
+    -   You can probably skip this step if you already have a OpenSSL installed on your system.
+    -   If you have OpenSSL installed and you cannot reproduce the issue, then go back and do this step to help confirm that it is a versioning issue.
+3. Ensure you have the [rust toolchain installed](https://rustup.rs/).
+4. Set the `OPENSSL_DIR` environment variable to the OpenSSL install directory created by the servo build.
     -   Use the UWP version.
     -   This will be in the servo repo directory under `.servo/msvc-dependencies/openssl/{version}/x64-windows-uwp`.
-4. Run `cargo build`
-5. Copy `libssl.dll` and `libcrypto.dll` to the output directory (`./target/debug`).
-6. Run `./target/debug/test_ws.exe`.
+5. Run `cargo build`
+6. Copy `libssl.dll` and `libcrypto.dll` to the output directory (`./target/debug`).
+7. Run `cargo run`.
+
+It will prompt for which URL to use. One is a normal insecure websocket (`ws`) and the other is secure (`wss`).
+
+For option `0`, a connection with the server will be established and any message you type will be echoed back to you.
+
+For option `1`, a connection will be attempted but will fail for some reason.
+From what I can tell from wireshark, no packets are sent when connecting over `wss` while over `ws` the connection will be established correctly.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 
-use ws::{ Factory, Sender, WebSocket, Handler, Error as WebSocketError };
+use ws::{ Factory, Sender, WebSocket, Handler, Error as WebSocketError, Message, Result as WebSocketResult };
 use url::Url;
+use std::io;
+use std::thread;
+use std::io::Write;
 
 #[derive(Clone)]
 struct Client {}
@@ -19,6 +22,18 @@ impl Factory for Client {
 }
 
 impl Handler for Client {
+    fn on_message(&mut self, message: Message) -> WebSocketResult<()> {
+        let res = match message {
+            Message::Text(message) => {
+                println!("Server: {}", message);
+                Ok(())
+            },
+            Message::Binary(_) => Ok(()),
+        };
+
+        res
+    }
+
     fn on_error(&mut self, err: WebSocketError) {
         println!("Error in WebSocket communication: {:?}", err);
     }
@@ -30,13 +45,49 @@ fn main() {
 
     let client = Client {};
     let mut ws = WebSocket::new(client).unwrap();
-    let url = Url::parse("wss://yeticgi.casualos.com/socket.io/?EIO=3&transport=websocket").unwrap();
+    
+    let stdin = io::stdin();
+    println!("Choose an option:");
+    println!("0) ws://echo.websocket.org <-- This one works");
+    println!("1) wss://echo.websocket.org <-- This one does not");
+    print!("Type an option: ");
+    io::stdout().flush().unwrap();
+
+    let mut choice = String::new();
+    stdin.read_line(&mut choice).unwrap();
+    let url = match choice.chars().next().unwrap() {
+        // This choice works
+        '0' => Url::parse("ws://echo.websocket.org").unwrap(),
+
+        // This choice fails
+        '1' => Url::parse("wss://echo.websocket.org").unwrap(),
+
+        _ => panic!("You did not choose between 0 and 1"),
+    };
 
     if let Err(e) = ws.connect(url) {
         println!("Failed to establish a WebSocket connection: {:?}", e);
         return;
     };
 
+    let ws_sender = ws.broadcaster();
+    println!("Got sender!");
+
+    thread::spawn(move || {
+        loop {
+            let mut input = String::new();
+            match stdin.read_line(&mut input) {
+                Ok(_) => {
+                    ws_sender.send(input).unwrap();
+                },
+                Err(error) => {
+                    println!("Input error: {}", error);
+                }
+            }
+        }
+    });
+    
+    println!("run!");
     if let Err(e) = ws.run() {
         println!("Failed to run WebSocket: {:?}", e);
     };
